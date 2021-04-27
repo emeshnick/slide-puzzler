6e585f210d222a66162fea17705ad19b9b33f047
Elijah Meshnick <emeshnick@gmail.com>
2021-04-27 16:30:20 -0400

    chore: render puzzle component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Puzzle from "./components/Puzzle";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Puzzle />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/components/Puzzle.js
+++ src/components/Puzzle.js
@@ -27,3 +27,5 @@ class Puzzle extends React.Component {
     );
   }
 }
+
+export default Puzzle;
